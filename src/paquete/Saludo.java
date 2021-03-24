388a5730127ebde81670d1638df1318ea1b8215b
CEINMARK <CEINMARK@DESKTOP-QOO9QN8>
2021-03-24 11:22:54 +0100

    hola

---
+++ src/paquete/Saludo.java
@@ -0,0 +1,16 @@
+package paquete;
+
+import java.util.Scanner;
+
+public class Saludo {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Buenos días Ceinmark, soy Jorge");
+		System.out.println("Introduce un número para averiguar si es primo o no:");
+		int num=sc.nextInt();
+		
+
+	}
+
+}
